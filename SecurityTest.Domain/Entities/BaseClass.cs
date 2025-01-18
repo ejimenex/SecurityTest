92606b621009030ff71d3af6001bee1d8fa2171d
ejimenex <edgarj.galvan@gmail.com>
2025-01-18 00:13:38 -0400

    Add generic class and some configuration before start to code

---
+++ SecurityTest.Domain/Entities/BaseClass.cs
@@ -0,0 +1,8 @@
+﻿namespace SecurityTest.Domain.Entities
+{
+    public class BaseClass
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
--- SecurityTest.Persistence/SecurityTestDataContext.cs
+++ SecurityTest.Persistence/SecurityTestDataContext.cs
@@ -1,7 +1,11 @@
-﻿namespace SecurityTest.Persistence
-{
-    public class SecurityTestDataContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace SecurityTest.Persistence
+{
+    public class SecurityTestDataContext:DbContext
+    {
+        public SecurityTestDataContext(DbContextOptions<SecurityTestDataContext> options) : base(options)
+        {
+        }
     }
 }
